03c4a57a755179d00f19d460aa94a57a16938852
Takashi Kitajima <inc@2inc.org>
2018-01-28 15:38:39 +0900

    Some fixes

--- src/App/template-tags/get-template-part.php
+++ src/App/template-tags/get-template-part.php
@@ -5,92 +5,7 @@
  * @license GPL-2.0+
  */
 
-/**
- * This class make function that like get_template_part().
- * In addition to calling templates, you can pass variables.
- */
-class Inc2734_WP_View_Controller_Template_Part {
-
-	/**
-	 * The template path like get_template_part()
-	 *
-	 * @var string
-	 */
-	protected $template;
-
-	/**
-	 * The slug like get_template_part()
-	 *
-	 * @var string
-	 */
-	protected $slug;
-
-	/**
-	 * Variables
-	 *
-	 * @var array
-	 */
-	protected $vars = [];
-
-	/**
-	 * WP_Query object
-	 *
-	 * @var WP_Query
-	 */
-	protected $wp_query;
-
-	/**
-	 * @param string $template
-	 * @param string $slug
-	 * @return void
-	 */
-	public function __construct( $template, $slug = '' ) {
-		global $wp_query;
-
-		$this->template = $template;
-		$this->slug     = $slug;
-		$this->wp_query = $wp_query;
-	}
-
-	/**
-	 * Sets a variable
-	 *
-	 * @param string $key
-	 * @param mixed $value
-	 * @return void
-	 */
-	public function set_var( $key, $value ) {
-		if ( null === $this->wp_query->get( $key, null ) || ! isset( $this->vars[ $key ] ) ) {
-			$this->vars[ $key ] = $value;
-			$this->wp_query->set( $key, $value );
-		}
-	}
-
-	/**
-	 * Sets variables
-	 *
-	 * @param array $vars
-	 * @return void
-	 */
-	public function set_vars( $vars ) {
-		foreach ( $vars as $key => $value ) {
-			$this->set_var( $key, $value );
-		}
-	}
-
-	/**
-	 * Rendering the template part
-	 *
-	 * @return void
-	 */
-	public function render() {
-		get_template_part( $this->template, $this->slug );
-		foreach ( $this->vars as $key => $value ) {
-			unset( $value );
-			$this->wp_query->set( $key, null );
-		}
-	}
-}
+use Inc2734\WP_View_Controller\App\Template_Part;
 
 /**
  * A template tag that is get_template_part() using variables
@@ -100,7 +15,7 @@ class Inc2734_WP_View_Controller_Template_Part {
  * @return void
  */
 function wpvc_get_template_part( $template, $vars = [] ) {
-	$template_part = new Inc2734_WP_View_Controller_Template_Part( $template );
+	$template_part = new Template_Part( $template );
 	$template_part->set_vars( $vars );
 	$template_part->render();
 }
--- tests/test-get-template-part.php
+++ tests/test-get-template-part.php
@@ -15,7 +15,7 @@ class Inc2734_WP_View_Controller_Template_Part_Test extends WP_UnitTestCase {
 	 */
 	public function set_var() {
 		global $wp_query;
-		$Template_Part = new Inc2734_WP_View_Controller_Template_Part( 'template' );
+		$Template_Part = new Inc2734\WP_View_Controller\App\Template_Part( 'template' );
 		$Template_Part->set_var( '_name', 'value' );
 		$this->assertEquals( 'value', $wp_query->get( '_name' ) );
 	}
@@ -25,7 +25,7 @@ class Inc2734_WP_View_Controller_Template_Part_Test extends WP_UnitTestCase {
 	 */
 	public function set_vars() {
 		global $wp_query;
-		$Template_Part = new Inc2734_WP_View_Controller_Template_Part( 'template' );
+		$Template_Part = new Inc2734\WP_View_Controller\App\Template_Part( 'template' );
 		$Template_Part->set_vars( array(
 			'_name-1' => 'value-1',
 			'_name-2' => 'value-2',
@@ -39,7 +39,7 @@ class Inc2734_WP_View_Controller_Template_Part_Test extends WP_UnitTestCase {
 	 */
 	public function render() {
 		global $wp_query;
-		$Template_Part = new Inc2734_WP_View_Controller_Template_Part( 'template' );
+		$Template_Part = new Inc2734\WP_View_Controller\App\Template_Part( 'template' );
 		$Template_Part->set_var( '_name', 'value' );
 		$Template_Part->render();
 		$this->assertEquals( '', $wp_query->get( '_name' ) );
