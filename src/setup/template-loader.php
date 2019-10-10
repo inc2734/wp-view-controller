c257d1dce7eeeea5ac3587df4604c4b7b23e2e7b
Takashi Kitajima <inc@2inc.org>
2019-10-10 09:16:44 +0900

    Fix #2

--- src/setup/template-loader.php
+++ src/setup/template-loader.php
@@ -13,7 +13,7 @@ use Inc2734\WP_View_Controller\Helper;
 add_action(
 	'after_setup_theme',
 	function() {
-		$types = array(
+		$types = [
 			'index',
 			'404',
 			'archive',
@@ -31,35 +31,11 @@ add_action(
 			'single',
 			'singular',
 			'attachment',
-		);
+		];
 
 		foreach ( $types as $type ) {
-			add_filter(
-				"{$type}_template_hierarchy",
-				function( $templates ) use ( $type ) {
-					$new_templates = $templates;
-
-					foreach ( $templates as $template ) {
-						if ( 'frontpage' === $type ) {
-							$_wp_page_template = get_post_meta( get_the_ID(), '_wp_page_template', true );
-							if ( $_wp_page_template && 'default' !== $_wp_page_template ) {
-								$new_templates = array_merge( [ $_wp_page_template ], $new_templates );
-								continue;
-							}
-						}
-					}
-
-					$default_template_index = array_search( 'index.php', $new_templates );
-					if ( false !== $default_template_index ) {
-						unset( $new_templates[ $default_template_index ] );
-						$new_templates[] = 'index.php';
-					}
-
-					return array_unique( $new_templates );
-				}
-			);
-
 			/**
+			 * Override query template with hierarchy template.
 			 * Only relative paths are stored in $templates.
 			 * Since only the files directly under the theme are checked, the extended root is also checked.
 			 */
@@ -67,30 +43,23 @@ add_action(
 				"{$type}_template",
 				function( $template, $type, $templates ) {
 					$located = Helper::locate_template( $templates, false );
-					if ( $located ) {
-						return $located;
-					}
-
-					$template_names = [];
-					$hierarchy = Helper::config( 'templates' );
-					foreach ( $hierarchy as $root ) {
-						foreach ( $templates as $_template ) {
-							$template_names = untrailingslashit( $root ) . '/' . $_template;
-						}
-					}
-					if ( $template_names ) {
-						$located = Helper::locate_template( $template_names, false );
-						if ( $located ) {
-							return $located;
-						}
-					}
-
-					return $template;
+					return $located ? $located : $template;
 				},
 				10,
 				3
 			);
 		}
+
+		add_filter(
+			"frontpage_template_hierarchy",
+			function( $templates ) {
+				$_wp_page_template = get_post_meta( get_the_ID(), '_wp_page_template', true );
+				if ( $_wp_page_template && 'default' !== $_wp_page_template ) {
+					$templates = array_merge( [ $_wp_page_template ], $templates );
+				}
+				return $templates;
+			}
+		);
 
 		add_filter(
 			'frontpage_template',
@@ -102,18 +71,7 @@ add_action(
 		add_filter(
 			'template_include',
 			function( $template ) {
-				$filename  = $template;
-				$hierarchy = Helper::get_template_part_root_hierarchy();
-				$hierarchy = array_merge( $hierarchy, [ get_stylesheet_directory(), get_template_directory() ] );
-				$hierarchy = array_unique( $hierarchy );
-
-				foreach ( $hierarchy as $root ) {
-					$filename = str_replace( trailingslashit( $root ), '', $filename );
-					$filename = preg_replace( '|^/*?([^/]+?)/*?$|', '$1', $filename );
-				}
-
-				$filtered_template = apply_filters( 'inc2734_wp_view_controller_controller', $template, $filename );
-
+				$filtered_template = apply_filters( 'inc2734_wp_view_controller_controller', $template );
 				return file_exists( $filtered_template ) ? $filtered_template : $template;
 			}
 		);
