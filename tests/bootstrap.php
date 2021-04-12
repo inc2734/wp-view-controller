7c6d6ca42891dea8082c232a39f1ab41f8e9dd38
inc2734 <inc@2inc.org>
2021-04-13 00:41:00 +0900

    some fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -39,7 +39,7 @@ define( 'GUTENBERG_LOAD_VENDOR_SCRIPTS', false );
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	switch_theme( 'TwentyTwenty' );
+	switch_theme( 'twentytwenty' );
 	require dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
